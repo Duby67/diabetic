563d38cb0376e4817edde66aa2b50af518403e8c
Duby67 <75853099+Duby67@users.noreply.github.com>
2024-12-19 19:47:34 +0300

    optimized

--- example.py
+++ example.py
@@ -1,32 +1,28 @@
-import pandas as pd 
-import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set_theme(style='darkgrid')
-import copy 
-import os 
-import torch
-from PIL import Image 
-from torch.utils.data import Dataset 
-import torchvision
-import torchvision.transforms as transforms 
-from torch.optim.lr_scheduler import ReduceLROnPlateau  
-import torch.nn as nn 
-from torchvision import utils 
-from torchvision.datasets import ImageFolder
-from torchsummary import summary
+from cv2 import destroyAllWindows as destroyWindows
+from cv2 import imshow as show
+from cv2 import waitKey as wait
+from cv2 import imread as read
+from cv2 import putText as put
+from cv2 import resize as resize
+from cv2 import FONT_HERSHEY_SIMPLEX as font
+from numpy import floor as floor
+from os.path import join as join
+from os import walk as walk
+from os import getcwd as getcwd
+from PIL.Image import open as openImage
+from torch import argmax as argmax
+from torch import softmax as softmax
+from torch import no_grad as nograd
+from torch import device as device
+from torch import load as load
+from torch.cuda import is_available as is_available
+import torch.nn.modules.module as module
+import torch.nn.modules.conv as conv2d
+import torch.nn.modules.linear as linear
 import torch.nn.functional as F
-from sklearn.metrics import classification_report
-import itertools 
-from tqdm.notebook import trange, tqdm 
-from torch import optim
-import warnings
-from torchvision.io import read_image
-warnings.filterwarnings('ignore')
 from torchvision import transforms as T
 
-class CNN_Retino(nn.Module):
+class CNN_Retino(module.Module):
     
     def __init__(self, params):
         
@@ -39,19 +35,19 @@ class CNN_Retino(nn.Module):
         self.dropout_rate = params["dropout_rate"] 
         
         # CNN Layers
-        self.conv1 = nn.Conv2d(Cin, init_f, kernel_size=3)
+        self.conv1 = conv2d.Conv2d(Cin, init_f, kernel_size=3)
         h,w=findConv2dOutShape(Hin,Win,self.conv1)
-        self.conv2 = nn.Conv2d(init_f, 2*init_f, kernel_size=3)
+        self.conv2 = conv2d.Conv2d(init_f, 2*init_f, kernel_size=3)
         h,w=findConv2dOutShape(h,w,self.conv2)
-        self.conv3 = nn.Conv2d(2*init_f, 4*init_f, kernel_size=3)
+        self.conv3 = conv2d.Conv2d(2*init_f, 4*init_f, kernel_size=3)
         h,w=findConv2dOutShape(h,w,self.conv3)
-        self.conv4 = nn.Conv2d(4*init_f, 8*init_f, kernel_size=3)
+        self.conv4 = conv2d.Conv2d(4*init_f, 8*init_f, kernel_size=3)
         h,w=findConv2dOutShape(h,w,self.conv4)
         
         # compute the flatten size
         self.num_flatten=h*w*8*init_f
-        self.fc1 = nn.Linear(self.num_flatten, num_fc1)
-        self.fc2 = nn.Linear(num_fc1, num_classes)
+        self.fc1 = linear.Linear(self.num_flatten, num_fc1)
+        self.fc2 = linear.Linear(num_fc1, num_classes)
 
     def forward(self,X):
         
@@ -75,14 +71,13 @@ def findConv2dOutShape(hin,win,conv,pool=2):
     padding=conv.padding
     dilation=conv.dilation
 
-    hout=np.floor((hin+2*padding[0]-dilation[0]*(kernel_size[0]-1)-1)/stride[0]+1)
-    wout=np.floor((win+2*padding[1]-dilation[1]*(kernel_size[1]-1)-1)/stride[1]+1)
+    hout = floor((hin+2*padding[0]-dilation[0]*(kernel_size[0]-1)-1)/stride[0]+1)
+    wout = floor((win+2*padding[1]-dilation[1]*(kernel_size[1]-1)-1)/stride[1]+1)
 
     if pool:
         hout/=pool
         wout/=pool
-    return int(hout),int(wout)
-
+    return int(hout), int(wout)
 
 def get_transform():
     transforms = []
@@ -91,46 +86,45 @@ def get_transform():
     transforms.append(T.Normalize(mean = [0.485, 0.456, 0.406],std = [0.229, 0.224, 0.225]))
     return T.Compose(transforms)
 
-dir_path = os.getcwd()
-test_path = os.getcwd() + "/testing"
-batch_size = 64
-model = torch.load(dir_path + "/Retino_model.pt")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
- 
+dir_path = getcwd()
+test_path = getcwd() + "/testing"
+model = load(dir_path + "/Retino_model.pt")
+Mydevice = device("cuda" if is_available() else "cpu")
+model = model.to(Mydevice)
 ImagesNames = []
 CvImages = []
 eval_transform = get_transform()
-model.eval()
-for root, dirs, imgs in os.walk(test_path):
+'''
+for root, dirs, imgs in walk(test_path):
         for img in imgs:
             ImagesNames.append(img)
-            img_path = os.path.join(root, img)
-            image = Image.open(img_path)
-            CVimage= cv.imread(img_path)
-            with torch.no_grad():
+            img_path = join(root, img)
+            image = openImage(img_path)
+            CVimage= read(img_path)
+            with nograd():
                 x = eval_transform(image)
-                x.to(device)
+                x.to(Mydevice)
                 predictions = model(x)
-                probabilities = torch.softmax(predictions, dim=1)
-                predicted_classes = torch.argmax(probabilities, dim=1)
+                probabilities = softmax(predictions, dim=1)
+                predicted_classes = argmax(probabilities, dim=1)
                 for predicted_class in predicted_classes:
                     if predicted_class.item() == 0:
                         label = "Diabetic retinopathy"
                     elif predicted_class.item() == 1:
                         label = "No diabetic retinopathy"
-                    CVimage = cv.resize(CVimage,(400,400))
-                    cv.putText(CVimage, label,  (20,20), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+                    CVimage = resize(CVimage,(400,400))
+                    put(CVimage, label,  (20,20), font, 1, (0,255,0), 2)
                     CvImages.append(CVimage)
+       
 i = 0
 fl = True
 while True:
     if fl:
-        cv.destroyAllWindows()
-        cv.imshow(str(ImagesNames[i]) + " Position " + str(i), CvImages[i])
-    key = cv.waitKey(0)
+        destroyWindows()
+        show(str(ImagesNames[i]) + " Position " + str(i), CvImages[i])
+    key = wait(0)
     if key == 27:
-        cv.destroyAllWindows()
+        destroyWindows()
         break
     if key == 81:
         if i > 0:
@@ -146,3 +140,4 @@ while True:
             fl = False
     else:
         fl = False
+'''   
